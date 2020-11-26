d05d0f395cf77dec91a9cb5d861d755bc4cd93f6
morfeusys <che@just-ai.com>
2020-11-26 19:34:39 +0300

    built-in manual

--- src/main/kotlin/com/justai/dialogflower/Server.kt
+++ src/main/kotlin/com/justai/dialogflower/Server.kt
@@ -2,6 +2,9 @@ package com.justai.dialogflower
 
 import com.justai.jaicf.channel.http.httpBotRouting
 import com.justai.jaicf.channel.yandexalice.AliceChannel
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
@@ -12,6 +15,12 @@ fun main() {
             httpBotRouting(
                     "/" to AliceChannel(dialogflower, System.getenv("OAUTH_TOKEN"))
             )
+            get("/") {
+                call.respondText(
+                        "<center>Скопируйте адрес этой страницы и используйте его в качестве Webhook URL в вашем навыке для Алисы</center>",
+                        ContentType.Text.Html
+                )
+            }
         }
     }.start(wait = true)
 }
